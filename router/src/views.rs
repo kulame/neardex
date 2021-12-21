8f12769f6b02e338b8bbab1af8a9dfedbe9d583a
kula <kula@live.com>
2021-12-21 15:47:08 +0800

    feat: 实现添加代币功能

--- router/src/views.rs
+++ router/src/views.rs
@@ -1,11 +1,27 @@
 use crate::*;
 use near_sdk::near_bindgen;
 
+#[derive(Serialize, Deserialize, PartialEq)]
+#[serde(crate = "near_sdk::serde")]
+#[cfg_attr(not(target_arch = "wasm32"), derive(Debug))]
+pub struct StorageState {
+    pub deposit: U128,
+    pub usage: U128,
+}
+
 #[near_bindgen]
 impl Contract {
     pub fn get_user_tokens(&self, account_id: ValidAccountId) -> Vec<AccountId> {
         self.internal_get_account(account_id.as_ref())
             .map(|x| x.get_tokens())
             .unwrap_or_default()
+    }
+
+    pub fn get_user_storage_state(&self, account_id: ValidAccountId) -> Option<StorageState> {
+        self.internal_get_account(account_id.as_ref())
+            .map(|x| StorageState {
+                deposit: U128(x.near_amount),
+                usage: U128(x.storage_usage()),
+            })
     }
 }
